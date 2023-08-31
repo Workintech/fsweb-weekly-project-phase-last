76fb015bf88bfc600a8a69e845560c9170c0f370
Gökhan <gn.ozdemir@gmail.com>
2023-08-31 21:40:11 +0300

    input handlers

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,42 @@ import './App.css';
 
 function App() {
   const [product, setProduct] = useState(productData);
-  const previewImage = product.properties.find(prp => prp.type === 'image').variants[0];
-  const [selectedPreview, setSelectedPreview] = useState(previewImage);
-  const [selectedVariants, setSelectedVariants] = useState({
-    // "Yaş Grubu": "children"
-  });
+  const previewImageDefault = product.properties.find(prp => prp.type === 'image').variants[0];
+  const [selectedPreview, setSelectedPreview] = useState(previewImageDefault);
+  const [selectedVariants, setSelectedVariants] = useState({});
 
-  // TODO: handleVariantChange fonksiyonunu tamamlayın.
+  // DONE: handleVariantChange fonksiyonunu tamamlayın.
   const handleVariantChange = (propertyLabel, variantValue) => {
     // Seçilen varyantları güncelleyin.
     // Örnek: setSelectedVariants(prev => ({ ...prev, [propertyLabel]: variantValue }));
+    setSelectedVariants({ ...selectedVariants, [propertyLabel]: variantValue })
+    /*  t0: const [state, setStage] = useState(0)
+     t0 state => 0
+     t1 setState(7)
+     t1 state => 7 
+     t2 setState(biFn(10))
+     t2 state => 12
+ 
+     stateHistory = {
+       t0: 0,
+       t1: 7,
+       t2: 12
+     }
+ 
+     setState((p)=>p+1)
+     setState (t2)=>t2+1)
+ 
+     t3: 13
+ 
+     stateHistory = {
+       t0: 0,
+       t1: 7,
+       t2: 12,
+       t3: 13
+     }
+     
+     // birArray.map((data, ind)=>data.ssdsfasdf)
+  */
   };
 
   // TODO: calculatePrice fonksiyonunu tamamlayın.
@@ -28,7 +54,10 @@ function App() {
       <div className='app--panel'>
 
         <div className='image-container'>
-          <img /* TODO: Seçilen görseli gösterin */ alt={`${product.title} - ${selectedPreview.label}`} />
+          <img
+            src={selectedPreview.imageUrl}
+            /* DONE: Seçilen görseli gösterin */
+            alt={`${product.title} - ${selectedPreview.label}`} />
         </div>
       </div>
       <div className='app--panel'>
@@ -43,7 +72,7 @@ function App() {
                   {prp.type === 'radio' &&
                     <>
                       <h3>{prp.label}</h3>
-                      {/* TODO: Radyo butonları için varyantları listeyin ve seçim yapılmasını sağlayın */}
+                      {/* DONE: Radyo butonları için varyantları listeyin ve seçim yapılmasını sağlayın */}
                       <div className='other-variants'>
                         {prp.variants.map((v, ind) => (
                           <div key={ind} className='variant-oth'>
@@ -52,7 +81,7 @@ function App() {
                                 type="radio"
                                 name={prp.label}
                                 id={v.label}
-                                /*checked= TODO: Radyo butonları için varyantları listeyin ve seçim yapılmasını sağlayın */
+                                /*checked= DONE: Radyo butonları için varyantları listeyin ve seçim yapılmasını sağlayın */
                                 checked={selectedVariants[prp.label] === v.value}
                                 onChange={() => handleVariantChange(prp.label, v.value)}
                                 value={v.value} />
@@ -70,10 +99,12 @@ function App() {
                       <label>
                         <select className='variant-drp'
                           onChange={e => handleVariantChange(prp.label, e.target.value)}
-                          name={prp.label}>
+                          name={prp.label}
+                          value={selectedVariants[prp.label] || ""}>
+
                           {prp.variants.map((v, ind) => (
-                            /* TODO: Dropdown için varyantları listeyin ve seçim yapılmasını sağlayın */
-                            <option></option>
+                            /* DONE: Dropdown için varyantları listeyin ve seçim yapılmasını sağlayın */
+                            <option key={ind} value={v.value}>{v.label}</option>
                           ))}
                         </select>
                       </label>
@@ -85,15 +116,15 @@ function App() {
 
                       <div className='image-variants'>
                         {prp.variants.map((v, ind) => (
-                          <div key={ind} className={`variant-img ${(v.imageUrl === selectedPreview.imageUrl) && "selected"}`}>
+                          <div key={ind} className={`variant-img ${selectedVariants[prp.label] === v.value && "selected"}`}>
                             <img
-                              /* onClick={() => {
-                                
-                                 setSelectedPreview(........);
-                                 handleVariantChange(prp.label, v.value);
-                               }}
-                               TODO: Görsel varyantları listeyin ve seçim yapılmasını sağlayın 
-                               src={...}*/
+                              onClick={() => {
+                                setSelectedPreview(v);
+                                handleVariantChange(prp.label, v.value);
+                              }}
+                              /*  DONE: Görsel varyantları listeyin ve seçim yapılmasını sağlayın 
+                              */
+                              src={v.buttonImage}
                               alt={`${v.label} resmindeki örneği görüntüle`
                               }
                             />
@@ -113,8 +144,10 @@ function App() {
                               type="radio"
                               name={prp.type}
                               value={v.value}
-                            /* checked={} 
-                               onChange={} */
+                              /* checked={} 
+                                 onChange={} */
+                              checked={selectedVariants[prp.label] === v.value}
+                              onChange={() => handleVariantChange(prp.label, v.value)}
                             />
                             <span className="checkmark" style={{ display: 'inline-block', backgroundColor: v.value }}></span>
                           </label>
